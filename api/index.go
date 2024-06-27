294a71449fb9d90567ad2eebb1e6d5a5a37cd321
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-06-27 06:51:11 +0100

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,28 +1,27 @@
 package handler
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/gofiber/adaptor/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
-	"log"
-	"encoding/json"
 	"github.com/daniwalter001/jackett_fiber/types"
 )
 
 // initApp initializes the Fiber app and returns it
 func initApp() *fiber.App {
 	// Load environment variables
-	err := godotenv.Load("./.env")
-	if err != nil {
-		log.Fatalln("Error loading .env file")
+	if err := godotenv.Load("./.env"); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
 	}
 
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(200).SendString("Working")
+		return c.Status(http.StatusOK).SendString("Working")
 	})
 
 	app.Get("/manifest.json", func(c *fiber.Ctx) error {
@@ -40,11 +39,11 @@ func initApp() *fiber.App {
 
 		response, err := json.Marshal(manifest)
 		if err != nil {
-			return c.Status(500).SendString(err.Error())
+			return c.Status(http.StatusInternalServerError).SendString(err.Error())
 		}
 
 		c.Set("Content-Type", "application/json")
-		return c.Status(200).Send(response)
+		return c.Status(http.StatusOK).Send(response)
 	})
 
 	return app
