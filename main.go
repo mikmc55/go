090abbd86e93e05a90b66c9aac84fad94265c605
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-06-27 05:53:49 +0100

    Update main.go

--- main.go
+++ main.go
@@ -16,11 +16,13 @@ import (
 	"github.com/anacrolix/torrent"
 	"github.com/daniwalter001/jackett_fiber/types"
 	"github.com/daniwalter001/jackett_fiber/types/rd"
+	"github.com/gofiber/adaptor/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 )
 
-func main() {
+// initApp initializes the Fiber app and returns it
+func initApp() *fiber.App {
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetEscapeHTML(false)
 	ctx := context.Background()
@@ -48,23 +50,17 @@ func main() {
 	})
 
 	app.Get("/manifest.json", func(c *fiber.Ctx) error {
-		a := types.StreamManifest{ID: "strem.go.beta", Description: "Random Golang version on stremio Addon", Name: "GoDon", Resources: []string{"stream"}, Version: "1.0.9", Types: []string{"movie", "series", "anime"}, Logo: "https://upload.wikimedia.org/wikipedia/commons/2/23/Golang.png", IdPrefixes: []string{"tt", "kitsu"}, Catalogs: []string{}}
-
-		// {
-		// 	"catalogs": [  ],
-		// 	"description": "VOD from Google Drive.",
-		// 	"id": "hy.stremio.googledrive",
-		// 	"logo": "https://raw.githubusercontent.com/mik25/stremio-greek-tv/master/pngwing.com.png",
-		// 	"name": "GDrive Reborn",
-		// 	"resources": [
-		// 	  "stream"
-		// 	],
-		// 	"types": [
-		// 	  "movie",
-		// 	  "series"
-		// 	],
-		// 	"version": "2.0.0"
-		//   }
+		a := types.StreamManifest{
+			ID:          "strem.go.beta",
+			Description: "Random Golang version on stremio Addon",
+			Name:        "GoDon",
+			Resources:   []string{"stream"},
+			Version:     "1.0.9",
+			Types:       []string{"movie", "series", "anime"},
+			Logo:        "https://upload.wikimedia.org/wikipedia/commons/2/23/Golang.png",
+			IdPrefixes:  []string{"tt", "kitsu"},
+			Catalogs:    []string{},
+		}
 
 		u, err := json.Marshal(a)
 		if err != nil {
@@ -93,7 +89,6 @@ func main() {
 		id := c.Params("id")
 		id = strings.ReplaceAll(id, "%3A", ":")
 
-		//Reading the cache
 		//Reading the cache
 		streams, err := rdClient.JSONGet(ctx, id, "$").Result()
 		if err == nil && streams != "" {
@@ -132,15 +127,6 @@ func main() {
 			}
 		}
 
-		// fmt.Println("----------------------------")
-		// fmt.Println(tt)
-		// fmt.Println(strconv.Itoa(s))
-		// fmt.Println(strconv.Itoa(e))
-		// fmt.Println(abs)
-		// fmt.Println(strconv.Itoa(abs_season))
-		// fmt.Println(strconv.Itoa(abs_episode))
-		// fmt.Println("----------------------------")
-
 		name, year := getMeta(tt, type_)
 
 		var results []types.ItemsParsed
@@ -160,8 +146,6 @@ func main() {
 		fmt.Printf("Requests: %d\n", l)
 
 		wg.Add(l)
-
-		//=========================================================
 
 		if type_ == "movie" {
 			go func() {
@@ -216,8 +200,6 @@ func main() {
 			}
 		}
 
-		//=========================================================
-
 		wg.Wait()
 
 		sort.Slice(results, func(i, j int) bool {
@@ -235,10 +217,6 @@ func main() {
 		if len(results) > maxRes {
 			results = results[:maxRes]
 		}
-
-		// for index, el := range results {
-		// 	fmt.Printf("%d. %s => %s\n", index, el.MagnetURI, el.Seeders)
-		// }
 
 		fmt.Printf("Retenus:%d\n", len(results))
 
@@ -265,7 +243,6 @@ func main() {
 		wg.Wait()
 
 		var parsedSuitableTorrentFiles []torrent.File
-		// var parsedSuitableTorrentFilesIndex = make([]int, len(parsedTorrentFiles))
 		var parsedSuitableTorrentFilesIndex = make(map[string]int, 0)
 
 		for index, el := range parsedTorrentFiles {
@@ -288,149 +265,42 @@ func main() {
 						containE_S(lower, strconv.Itoa(s), strconv.Itoa(e), abs == "true", strconv.Itoa(abs_season), strconv.Itoa(abs_episode)) ||
 						(s == 1 && (containsAbsoluteE(lower, strconv.Itoa(s), strconv.Itoa(e), true, strconv.Itoa(s), strconv.Itoa(e)) ||
 							containsAbsoluteE_(lower, strconv.Itoa(s), strconv.Itoa(e), true, strconv.Itoa(s), strconv.Itoa(e)))) ||
-						// false ||
 						(((abs == "true" && containsAbsoluteE(lower, strconv.Itoa(s), strconv.Itoa(e), true, strconv.Itoa(abs_season), strconv.Itoa(abs_episode))) ||
-							(abs == "true" && containsAbsoluteE_(lower, strconv.Itoa(s), strconv.Itoa(e), true, strconv.Itoa(abs_season), strconv.Itoa(abs_episode)))) &&
-							!(strings.Contains(lower, "s0") && strings.Contains(lower, "e0") && strings.Contains(lower, "season") && strings.Contains(lower, fmt.Sprintf("s%d", abs_season)) && strings.Contains(lower, fmt.Sprintf("e%d", abs_episode))))) {
+							(abs == "true" && containsAbsoluteE_(lower, strconv.Itoa(s), strconv.Itoa(e), true, strconv.Itoa(abs_season), strconv.Itoa(abs_episode)))))) {
 						parsedSuitableTorrentFiles = append(parsedSuitableTorrentFiles, ell)
 						parsedSuitableTorrentFilesIndex[ell.DisplayPath()] = _index + 1
+
 						break
 					}
 				}
 			}
+
 			parsedTorrentFiles[index].TorrentData = parsedSuitableTorrentFiles
 		}
 
-		var ttttt types.StreamMeta
+		fmt.Printf("Results filtered:%d\n", len(parsedTorrentFiles))
 
-		parsedTorrentFiles = filter[types.ItemsParsed](parsedTorrentFiles, func(ip types.ItemsParsed) bool {
-			return len(ip.TorrentData) != 0
-		})
-
-		fmt.Printf("Response %d\n", len(parsedTorrentFiles))
-
-		fmt.Println("Parsing that shit")
-
-		wg = sync.WaitGroup{}
-		wg.Add(len(parsedTorrentFiles))
-
-		nbreAdded := 0
-
-		for _, el := range parsedTorrentFiles {
-			go func(item types.ItemsParsed) {
-				defer wg.Done()
-				for _, ell := range el.TorrentData {
-					fmt.Println(ell.DisplayPath())
-					if !isVideo(ell.DisplayPath()) {
-						continue
-					}
-
-					// ========================== RD =============================
-					fmt.Printf("Trynna some RD...\n")
-
-					infoHash := ell.Torrent().InfoHash().String()
-					// magnet := fmt.Sprint(ell.Torrent().Metainfo().Magnet(nil, ell.Torrent().Info()))
-					var folderId string
-					var details []rd.UnrestrictLinkResponse
-					var data rd.AddTorrentResponse
-
-					available, err := checkTorrentFileinRD(infoHash)
-
-					if err.Error != "" {
-						continue
-					}
-
-					v, availableCheck := available[infoHash]
-
-					if !availableCheck {
-						continue
-					}
-
-					v_ := v["rd"]
-					availableCheck = len(v_) > 0
-
-					if availableCheck || nbreAdded < 3 {
-						data, err = addTorrentFileinRD2(fmt.Sprintf("magnet:?xt=urn:btih:%s", infoHash))
-						if availableCheck {
-							fmt.Println("Cached")
-						} else {
-							fmt.Println("Added")
-							nbreAdded = nbreAdded + 1
-						}
-					}
-
-					folderId = data.ID
-					selected, err := selectFilefromRD(folderId, "all")
-					if folderId != "" && selected {
-						torrentDetails, err_ := getTorrentInfofromRD(folderId)
-						//fmt.Println((PrettyPrint(torrentDetails)))
-						if err.Error != "" {
-							fmt.Println("Error")
-							fmt.Println(err_.Error)
-						}
-						var files []rd.Files
-						if len(torrentDetails.Files) > 0 {
-							files = filter[rd.Files](torrentDetails.Files, func(f rd.Files) bool {
-								return f.Selected == 1
-							})
-							links := torrentDetails.Links
-							selectedIndex := 0
-
-							if len(files) > 1 {
-								selectedIndex = slices.IndexFunc[[]rd.Files](files, func(t rd.Files) bool {
-									return strings.Contains(strings.ToLower(t.Path), strings.ToLower(ell.DisplayPath()))
-								})
-							}
-							if selectedIndex == -1 || len(links) <= selectedIndex {
-								selectedIndex = 0
-							}
-							if len(links) > 0 {
-								unrestrictLink, errun := unrestrictLinkfromRD(links[selectedIndex])
-								details = append(details, unrestrictLink)
-								// fmt.Println(PrettyPrint(errun.Error))
-								// fmt.Println(PrettyPrint(details[len(details)-1]))
-								if errun.Error != "" {
-									continue
-								}
-							}
-						}
-
-					}
-
-					if len(details) > 0 {
-						ttttt.Streams = append(ttttt.Streams, types.TorrentStreams{Title: fmt.Sprintf("%s\n%s\n%s | %s", ell.Torrent().Name(), ell.DisplayPath(), getQuality(ell.DisplayPath()), getSize(int(ell.Length()))), Name: fmt.Sprintf("RD.%s\n S:%s, P:%s", item.Tracker, item.Seeders, item.Peers), Type: type_, BehaviorHints: types.BehaviorHints{BingeGroup: fmt.Sprintf("Jackett|%s", ell.Torrent().InfoHash().String()), NotWebReady: true}, URL: details[0].Download})
-
-						// ========================== END RD =============================
-					} else if os.Getenv("PUBLIC") == "1" {
-						announceList := make([]string, 0)
-						for i := 0; i < len(ell.Torrent().Metainfo().AnnounceList); i++ {
-							for j := 0; j < len(ell.Torrent().Metainfo().AnnounceList[i]); j++ {
-								announceList = append(announceList, fmt.Sprintf("tracker:%s", ell.Torrent().Metainfo().AnnounceList[i][j]))
-							}
-						}
-						announceList = append(announceList, fmt.Sprintf("dht:%s", ell.Torrent().InfoHash().String()))
-						ttttt.Streams = append(ttttt.Streams, types.TorrentStreams{Title: fmt.Sprintf("%s\n%s\n%s | %s", ell.Torrent().Name(), ell.DisplayPath(), getQuality(ell.DisplayPath()), getSize(int(ell.Length()))), Name: fmt.Sprintf("%s\n S:%s, P:%s", item.Tracker, item.Seeders, item.Peers), Type: type_, InfoHash: ell.Torrent().InfoHash().String(), Sources: announceList, BehaviorHints: types.BehaviorHints{BingeGroup: fmt.Sprintf("Jackett|%s", ell.Torrent().InfoHash().String()), NotWebReady: true}, FileIdx: parsedSuitableTorrentFilesIndex[ell.DisplayPath()] - 1})
-					}
-
-				}
-			}(el)
-		}
-
-		wg.Wait()
-
-		if len(ttttt.Streams) > 0 {
-			jsonBytes, errttt := json.Marshal(ttttt)
-			if errttt == nil {
-				_, errrrr := rdClient.JSONSet(ctx, id, "$", jsonBytes).Result()
-				if errrrr == nil {
-					rdClient.Expire(ctx, id, time.Hour*24*7).Result()
+		var streams_ = make([]types.StreamMeta, 0)
+		for _, element := range parsedTorrentFiles {
+			if len(element.TorrentData) > 0 {
+				for _, el := range element.TorrentData {
+					torrent := types.StreamMeta{Title: element.Title, InfoHash: element.InfoHash, FileIdx: parsedSuitableTorrentFilesIndex[el.DisplayPath()], BehaviorHints: types.BehaviorHints{BingeGroup: fmt.Sprintf("group-%s", tt), CountryWhitelist: []string{"en"}}}
+					streams_ = append(streams_, torrent)
 				}
 			}
 		}
 
-		fmt.Println("Sending that shit")
-		return c.Status(fiber.StatusOK).JSON(ttttt)
+		//Updating the cache
+		errCache := rdClient.JSONSet(ctx, id, "$", streams_).Err()
+		if errCache != nil {
+			fmt.Println(errCache)
+		}
+
+		return c.Status(fiber.StatusOK).JSON(streams_[len(streams_)-1])
 	})
 
-	app.Listen(":8000")
+	return app
 }
+
+// Handler is the exported function for Vercel
+var Handler = adaptor.FiberApp(initApp())
